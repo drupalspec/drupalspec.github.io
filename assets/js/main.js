8dae42d7597bd162db1ee578ce0f4813d52d1d66
rgolubov <roman@cloudconsulting.no>
2016-03-31 17:07:26 +0300

    commit message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(function() {
     $('ul.sidebar-nav ul li.active').parent().parent().addClass('active');
     $('ul.sidebar-nav li.active > ul').addClass('expanded');
+    $('ul.sidebar-nav li.active > ul').parent().addClass('expanded');
 });
