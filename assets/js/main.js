6235553bf6100a723c2efac9ab9ce7a34fc29711
user1 <roman@cloudconsulting.no>
2016-05-11 10:46:57 +0300

    aa

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(function() {
+    $('ul.sidebar-nav ul li.active').parent().parent().addClass('active');
+    $('ul.sidebar-nav li.active > ul').addClass('expanded');
+    $('ul.sidebar-nav li.active > ul').parent().addClass('expanded');
+
+
+    $('input:radio[name="reason"]').change(function() {
+        $('.reason').toggle();
+        $('.reasonon').toggle();
+    });
+
+    $('input:radio[name="reason2"]').change(function() {
+        $('.reason2').toggle();
+    });
+
+    $('input:radio[name="notify"]').change(function() {
+        $('.notify').toggle();
+    });
+
+    $('input:radio[name="notify4"]').change(function() {
+        $('.notify4').toggle();
+    });
+
+    $('input:radio[name="notify3"]').change(function() {
+        $('.notify3').toggle();
+        $('.notify2').toggle();
+    });
+
+    // $( "section.even label:even" ).css( "border-bottom", "4px solid #FBF9F9" );
+});
