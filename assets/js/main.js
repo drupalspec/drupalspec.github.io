3c38bc9e95992a823900620e3c6d1265b2b9f848
user1 <roman@cloudconsulting.no>
2016-05-17 18:05:56 +0300

    UPDATE mockup

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,5 +26,19 @@ $(function() {
         $('.notify2').toggle();
     });
 
+    //Modal
+    $('.popup-modal').magnificPopup({
+      type: 'inline',
+      preloader: false,
+      focus: '#username',
+      modal: true
+    });
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+      e.preventDefault();
+      $.magnificPopup.close();
+    });
+
     // $( "section.even label:even" ).css( "border-bottom", "4px solid #FBF9F9" );
 });
+
+
