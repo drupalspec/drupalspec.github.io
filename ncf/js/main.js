0d2b1166cb851e9fb106de743158591a2dfe5638
Seigiard <seigiard@gmail.com>
2016-10-10 16:57:06 +0300

    UPDATE NCF

--- ncf/js/main.js
+++ ncf/js/main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Chosen
 $(function() {
     $(".header-filter select").chosen({
@@ -6,13 +8,19 @@ $(function() {
 });
 
 // Calendar popup
+var calendarClass = '.icon-calendar, body,.calendar,.events-row';
 $(function() {
     $('.icon-calendar').click(function() {
-        $(this).toggleClass('active');
-        $('.calendar').toggleClass('active');
-        $('.events-row .event:first-child').toggleClass('active');
-        $('.icon-plus-squared').toggle();
+        $(calendarClass).addClass('active');
+        $('.icon-plus-squared, .overlay-calendar').show();
+        $('.login').hide();
+        $('.header-filter, .header-filter button').hide();
         return false;
+    });
+
+    $('.overlay-calendar, td.event').click(function() {
+        $('.overlay-calendar').hide();
+        $(calendarClass).removeClass('active');
     });
 
     $('.calendar .switch a').click(function() {
@@ -24,4 +32,38 @@ $(function() {
         }
         return false;
     });
+});
+
+// Filter events
+$('.header .menu-toggle').click(function() {
+    $('.header-filter, .header-filter button').show();
+    $(calendarClass).removeClass('active');
+    $('.overlay').show();
+    $('.login').hide();
+    return false;
+});
+
+
+// Login events
+$('.icon-logout').click(function() {
+    $('.overlay, .login').show();
+    $('.calendar').removeClass('active');
+    $('.header-filter, .header-filter button').hide();
+    $('.add-event').hide();
+});
+
+//Add new event
+$('.icon-plus-squared').click(function() {
+    $('.overlay, .add-event').show();
+    $('.calendar').removeClass('active');
+    $('.login').hide();
+    $('.header-filter, .header-filter button').hide();
+});
+
+// Global
+$('.overlay, .close, .login button').click(function() {
+    $(calendarClass).removeClass('active');
+    $('.overlay').toggle();
+    $('.login, .add-event').hide();
+    $('.header-filter, .header-filter button').hide();
 });
